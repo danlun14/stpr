a70dba9e9cb4f5dedf1028b1810a526d25a53533
Lunev Danila <danlun14@mail.ru>
2021-04-14 15:10:36 +0700

    create gaming field(class Board) with buttons

--- src/Game.js
+++ src/Game.js
@@ -10,11 +10,43 @@ class Square extends React.Component {
     }
 }
 
+class Board extends React.Component {
+    renderSquare() {
+        return <Square />;
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="playing-board">
+                    <div className="board-row">
+                        {this.renderSquare()}
+                        {this.renderSquare()}
+                        {this.renderSquare()}
+                    </div>
+                    <div className="board-row">
+                        {this.renderSquare()}
+                        {this.renderSquare()}
+                        {this.renderSquare()}
+                    </div>
+                    <div className="board-row">
+                        {this.renderSquare()}
+                        {this.renderSquare()}
+                        {this.renderSquare()}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
 class Game extends React.Component {
     render() {
         return (
-            <div className="game-board">
-                <Square />
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
             </div>
         );
     }
