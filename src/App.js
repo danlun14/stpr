46d8cf7ed145ea50e4904c13d5be11bd6f8c6166
Lunev Danila <danlun14@mail.ru>
2021-04-30 23:00:03 +0700

    add button> which starts game

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import './App.css';
 import Game from './Game'
 import Introduction from './Introduction'
-
+import './Introduction.css';
 import React from 'react';
 
 
-
+function StartGame(props) {
+  return (
+    <button className="startButton" onClick={props.click}>
+      <div className="startButtonContent">Start</div>
+    </button >
+  );
+}
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameStart: 0
+    };
+  }
+  handleStartGameClick() {
+    this.setState({
+
+      gameStart: 1
+    });
+  }
+
+  renderStartGameButton() {
+    return (
+      <div>
+        <Introduction />
+        <div className="rules"><StartGame click={() => this.handleStartGameClick()} /></div>
+      </div>
+    );
+  }
   render() {
-    return (<div>
-      <Introduction />
-      <Game />
-    </div>);
+    if (this.state.gameStart === 0) {
+      return this.renderStartGameButton();
+    } else {
+      return (<div>
+        <Game />
+      </div>);
+    }
   }
 }
 
